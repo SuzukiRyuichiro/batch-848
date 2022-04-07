8dae1b34399605b6e56702636d5a81e09bc23120
Scooter <dragon.aka.scooter@gmail.com>
2022-04-07 21:25:07 +0900

    created the file

---
+++ livecode/acronym/acronym.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Today's objective
+# 1. write tests using rspec
+# 2. refactor the code
+# 3. separate the files
+
+def acronymize(sentence)
+  # TODO: make acroynm out of the sentence given
+end
+
+# Test cases
+# Work from home => WFH
+# Test driven development => TDD
+# Ruby on rails => ROR
+# "" => ""
