6396bf3b6d6c3f2404a3b6d0fbb1c68d55fb6d5f
Scooter <dragon.aka.scooter@gmail.com>
2022-04-08 15:24:31 +0900

    Livecode done

--- livecode/acronym/acronym.rb
+++ livecode/acronym/acronym.rb
@@ -1,16 +1,12 @@
-# frozen_string_literal: true
-
 # Today's objective
-# 1. write tests using rspec
+# 1. write tests using rspec done
 # 2. refactor the code
 # 3. separate the files
 
 def acronymize(sentence)
   # TODO: make acroynm out of the sentence given
+  # Split the sentence into words => Array of words
+  # Get the first letters of this words => Array
+  # Join those letters -> upcase
+  sentence.split(" ").map { |word| word[0] }.join.upcase
 end
-
-# Test cases
-# Work from home => WFH
-# Test driven development => TDD
-# Ruby on rails => ROR
-# "" => ""
---
+++ livecode/acronym/spec/acronym_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../acronym'
+
+describe "#acronymize" do
+  it "should return 'WFH' when given 'Work from home'" do
+    expected = 'WFH'
+    actual = acronymize('Work from home')
+    expect(actual).to eq(expected)
+  end
+
+  it "should return 'TDD' when given 'Test driven development'" do
+    expected = 'TDD'
+    actual = acronymize('Test driven development')
+    expect(actual).to eq(expected)
+  end
+
+  it "should return empty string when given an empty string" do
+    expected = ''
+    actual = acronymize('')
+    expect(actual).to eq(expected)
+  end
+end
+x 
